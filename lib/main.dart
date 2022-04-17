00bed0544844bc6ab38b1a166214d1bc0fed24b6
fatkurfaris <@fatkurrizal12@gmail.com>
2022-04-17 12:27:18 +0700

    dcoding_2_statelesswidget

---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        body: Center(
+            // child: Heading(
+            //   // mengubah widget Text
+            //   text: "Hello world!",
+            // ),
+            child: Text("Hello world")),
+      ),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+
+  Heading({required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      style: TextStyle(
+        fontSize: 24.0,
+        fontWeight: FontWeight.bold,
+      ),
+    );
+  }
+}
